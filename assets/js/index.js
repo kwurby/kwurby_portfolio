8c97c07017cf4857afba5495e7f715e991c90317
DaKirbi <pastelicv@gmail.com>
2022-11-08 20:48:55 -0600

    almost DONE!!!!!!!

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,9 +10,23 @@ window.onload = function(){
     var style = getComputedStyle(document.body);
     dev(style.getPropertyValue('--secondary'));
 
-    const el = document.querySelectorAll(".link");
+    var el = document.querySelectorAll(".link");
     for (var i = 0 ; i < el.length; i++) {
         const annotation = RoughNotation.annotate(el[i], { type: 'underline', color: style.getPropertyValue('--secondary'), padding: 0});
         annotation.show();
     }
+
+    el = document.querySelectorAll(".highlight");
+    for (var i = 0 ; i < el.length; i++) {
+        const annotation = RoughNotation.annotate(el[i], { type: 'highlight', color: 'yellow', iterations: 1, multiline: true});
+        annotation.show();
+    }
+
+    el = document.querySelectorAll(".bracket");
+    for (var i = 0 ; i < el.length; i++) {
+        const annotation = RoughNotation.annotate(el[i], { type: 'bracket', color: 'red', padding: [2, 4], brackets: ['left', 'right']});
+        annotation.show();
+    }
+
+
 }
