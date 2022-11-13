50822b2d3d22a6f9fed868f165ab97812ccd6d66
DaKirbi <pastelicv@gmail.com>
2022-11-13 09:19:58 -0600

    bug fix
    made the notation load in when it is on the screen and made the website more mobile friendly. and added comments to the code.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,28 +5,53 @@ function dev(input){
     console.log(input);
     return true;
 }
+class RoughNotationStyling {
+    constructor(el, annotate) {
+        this.el = el
+        this.annotate = annotate
+        this.one = 0;
+    }
+    Displaylistener(){
+        //Displays if already on screen
+        if (this.el.getBoundingClientRect().bottom <= window.innerHeight && !this.one) {
+            this.one = 1;
+
+            //!can't declare in constructor because you will miss the animation.
+            this.annotation = RoughNotation.annotate(this.el, this.annotate);
+            this.annotation.show();
+        }
+        //waits till on screen
+        document.addEventListener("scroll", () => {
+            if (this.el.getBoundingClientRect().bottom <= window.innerHeight && !this.one) {
+                this.one = 1;
+                this.annotation = RoughNotation.annotate(this.el, this.annotate);
+                this.annotation.show();
+            }
+        });
+    }
+}
 
 window.onload = function(){
     var style = getComputedStyle(document.body);
-    dev(style.getPropertyValue('--secondary'));
 
-    var el = document.querySelectorAll(".link");
-    for (var i = 0 ; i < el.length; i++) {
-        const annotation = RoughNotation.annotate(el[i], { type: 'underline', color: style.getPropertyValue('--secondary'), padding: 0});
-        annotation.show();
+    //selectors for the animations
+    selectors = [
+        ".link",
+        ".highlight",
+        ".bracket"
+    ]
+
+    //reads threw the selectors
+    for (var i = 0; i < selectors.length; i++) {
+        current = selectors[i];
+        var el = document.querySelectorAll(selectors[i]);
+
+        //reads threw elements of selector type
+        for (var u = 0 ; u < el.length; u++) {
+            //new RoughNotationStyling( element, animation settings ).Displaylistener();
+            if (current == ".link") new RoughNotationStyling(el[u], { type: 'underline', color: style.getPropertyValue('--secondary'), padding: 0}).Displaylistener();
+            if (current == ".highlight") new RoughNotationStyling(el[u], { type: 'highlight', color: 'yellow',  iterations: 1, multiline: true}).Displaylistener();
+            if (current == ".bracket") new RoughNotationStyling(el[u], { type: 'bracket', color: 'red', padding: [2, 4], brackets: ['left', 'right']}).Displaylistener();
+        }
     }
-
-    el = document.querySelectorAll(".highlight");
-    for (var i = 0 ; i < el.length; i++) {
-        const annotation = RoughNotation.annotate(el[i], { type: 'highlight', color: 'yellow', iterations: 1, multiline: true});
-        annotation.show();
-    }
-
-    el = document.querySelectorAll(".bracket");
-    for (var i = 0 ; i < el.length; i++) {
-        const annotation = RoughNotation.annotate(el[i], { type: 'bracket', color: 'red', padding: [2, 4], brackets: ['left', 'right']});
-        annotation.show();
-    }
-
-
 }
